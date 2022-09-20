d79880712b9918ddd5b0198573c6d5414e12864c
Yousef Saeed <107230184+yousafesaeed@users.noreply.github.com>
2022-09-20 17:22:15 +0200

    Update config

--- app-frontend/bin/deploy.sh
+++ app-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-# aws s3 cp --recursive --acl public-read ./src s3://random-yousef-bucket/
-# aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./src/index.html s3://random-yousef-bucket/
+aws s3 cp --recursive --acl public-read ./src s3://random-yousef-bucket/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./src/index.html s3://random-yousef-bucket/
